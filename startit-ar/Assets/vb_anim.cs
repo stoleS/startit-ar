489a2842ab7df2287cc57e9330ca91124d8dda07
stoleS <white.knight0857@gmail.com>
2018-11-24 21:41:32 +0100

    Object spawn on ar button trigger

---
+++ startit-ar/Assets/vb_anim.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject vbBtnObj;
+    public GameObject canvas;
+    public Animator sphereAni;
+    private bool spawned = false;
+
+	// Use this for initialization
+	void Start () {
+        vbBtnObj = GameObject.Find("spawnButton");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        sphereAni.GetComponent<Animator>();
+        sphereAni.speed = 0f;
+	}
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        
+        if(!spawned)
+        {
+            sphereAni.Play("sphere_anim", -1, 0f);
+            sphereAni.speed = 1f;
+            spawned = true;
+        } else
+        {
+            sphereAni.Play("sphere_anim_exit", -1, 0f);
+            spawned = false;          
+        }
+  
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) { }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
